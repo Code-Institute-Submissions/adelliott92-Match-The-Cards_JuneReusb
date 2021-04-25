dfe9a6f4c7b649846b0a80ed509527dd3ecac1e5
ToastAndTeaLover <andrewdavidelliott@outlook.com>
2021-04-25 16:35:06 +0100

    feat: add dark mode functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	
+    $('.toggler').click(function(){
+        $(this).toggleClass('toggler-on white-background');
+        $('body').toggleClass('dark-background');
+        $('.dark-mode-button').toggleClass('grey-background');
+    })
 })
